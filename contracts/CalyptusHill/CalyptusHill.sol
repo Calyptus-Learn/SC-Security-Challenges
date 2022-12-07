8b73efeb0dbb8c5b2936d6a78ec6a73670a7e7aa
nizhunt <nizhunt.eth@gmail.com>
2022-12-07 13:40:33 +0530

    changes in readme

--- contracts/CalyptusHill/CalyptusHill.sol
+++ contracts/CalyptusHill/CalyptusHill.sol
@@ -4,20 +4,20 @@ pragma solidity ^0.8.0;
 contract CalyptusHill {
 
   address atTheTop;
-  uint public prize;
+  uint public bribe;
   address public owner;
 
   constructor() payable {
     owner = msg.sender;  
     atTheTop = msg.sender;
-    prize = msg.value;
+    bribe = msg.value;
   }
 
   receive() external payable {
-    require(msg.value >= prize || msg.sender == owner);
+    require(msg.value >= bribe || msg.sender == owner);
     payable(atTheTop).transfer(msg.value);
     atTheTop = msg.sender;
-    prize = msg.value;
+    bribe = msg.value;
   }
 
   function top() public view returns (address) {
